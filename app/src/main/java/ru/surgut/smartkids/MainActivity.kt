02f509dfe775c48fb09e2790ffc38c8050db4b3b
Azizbek <zizich@mail.ru>
2025-09-17 21:34:48 +0500

    Edit file MainActivity.kt

--- app/src/main/java/ru/surgut/smartkids/MainActivity.kt
+++ app/src/main/java/ru/surgut/smartkids/MainActivity.kt
@@ -1,47 +1,18 @@
-package ru.surgut.smartkids
+package ru.surgut.smartkid
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.surgut.smartkids.ui.theme.SmartKidsTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SmartKidsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btnStartTracking)
+        btnStart.setOnClickListener {
+            // Запускаем сервис геолокации
+            startService(Intent(this, LocationService::class.java))
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartKidsTheme {
-        Greeting("Android")
     }
 }
